8c6987e73ed4a65fff0a4c5a8b30f0375f0d066d
Tam Nguyen <hacaoideas@gmail.com>
2019-08-22 14:46:26 +0700

    clean up

--- src/examiner.py
+++ src/examiner.py
@@ -5,26 +5,15 @@ import loader
 
 
 class examiner:
-    """This class object loads and examines the data"""
-
-    #This will create the examiner and
-    #loads the data and
-    # def __init__(self, path_data_set):
-    #     self.raw_data = pd.read_csv(path_data_set, sep=',', header=None)
-    #     self.label_only = np.atleast_2d(self.raw_data)[:,0]
-    #     self.data_only = np.atleast_2d(self.raw_data)[:,1:]
-        
-        
-    #     self.data = np.array(self.raw_data)
+    """This class object examines the data, and make decision to feed the data
+    into the neuralNetwork"""
 
     def __init__(self, ld):
         self.data_only = ld.data_only
         self.label_only = ld.label_only
                 
-
-
     
-    #This load the first line and plot it
+    #This load the first line and plot it for testing purpose only
     def plotone(self, row):
         plt.imshow(self.data_only[row,:].reshape(28,28), cmap='Greys', interpolation=None)
         plt.savefig('../temp/sample_plot.png')
@@ -33,9 +22,8 @@ class examiner:
 
 
 if __name__ == "__main__":
-    ld = loader.loader('../data/mnist_train_100.csv')
-    ex = examiner(ld)
-    
-    ex.plotone(6)
-    print(ex.label_only[6])
+    ex = examiner(loader.loader('../data/mnist_train_100.csv'))
+
+    ex.plotone(7)
+    print(ex.label_only[7])
     
